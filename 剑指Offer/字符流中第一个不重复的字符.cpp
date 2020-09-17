cf5b188e9e8af04e45c5345205f39f68cdd299e0
GochenRyan <rangaocheng@outlook.com>
2020-09-17 13:31:59 +0800

    字符流中第一个不重复的字符

---
+++ 剑指Offer/字符流中第一个不重复的字符.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    Solution(): index(0)
+    {
+        for (int i = 0; i < 256; ++i)
+            occurrence[i] = -1;
+    }
+    
+  //Insert one char from stringstream
+    void Insert(char ch)
+    {
+        if (occurrence[ch] == -1)
+            occurrence[ch] = index;
+        else if (occurrence[ch] >= 0)
+            occurrence[ch] = -2;
+        ++index;
+    }
+  //return the first appearence once char in current stringstream
+    char FirstAppearingOnce()
+    {
+        char ch = '#';
+        int minIndex = numeric_limits<int>::max();
+        for (int i = 0; i < 256; ++i)
+        {
+            if (occurrence[i] >= 0 && occurrence[i] < minIndex)
+            {
+                ch = (char)i;
+                minIndex = occurrence[i];
+            }
+        }
+        return ch;
+    }
+
+private:
+    /*
+        -1:  未出现
+        -2： 出现多次
+        >=0: 出现一次
+    */
+    int occurrence[256];
+    int index;
+};
